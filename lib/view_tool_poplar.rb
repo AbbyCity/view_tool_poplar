a13e7bafbb902e9356681bf0ef2545a930bdd3b9
Jordan Hudgens <jordan@devcamp.com>
2017-07-17 15:34:36 +0000

    Dev tool initial commit

---
+++ lib/view_tool_poplar.rb
@@ -0,0 +1,6 @@
+require "view_tool_poplar/version"
+requier "View_tool_poplar/renderer"
+
+module ViewToolPoplar
+  # Your code goes here...
+end
---
+++ lib/view_tool_poplar/version.rb
@@ -0,0 +1,3 @@
+module ViewToolPoplar
+  VERSION = "0.1.0"
+end
